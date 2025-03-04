985a9619f29e60d2f48c638eb220afadb0b81f92
julesV <julesvallet4@gmail.com>
2025-03-04 11:39:02 +0100

    login page sans fonction

--- front/coursDevOps/src/app/app.component.ts
+++ front/coursDevOps/src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductCardComponent } from "./components/product-card/product-card.component";
-import { SearchBarComponent } from "./components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'coursDevOps';
-
-  onSearch(event: any){
-    console.log(event);
-  }
+  title = 'Graine de folie';
 }
---
+++ front/coursDevOps/src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+  protected isPasswordVisible: boolean = false;
+
+  protected togglePassword() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+}
