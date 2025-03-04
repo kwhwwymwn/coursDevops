00b5844499da0f52a47a22db92cabb6d8bcebc93
julesV <julesvallet4@gmail.com>
2025-03-04 09:45:29 +0100

    card product

--- front/coursDevOps/src/app/app.component.ts
+++ front/coursDevOps/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductCardComponent } from "./components/product-card/product-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- front/coursDevOps/src/app/components/product-card/product-card.component.ts
+++ front/coursDevOps/src/app/components/product-card/product-card.component.ts
@@ -1,16 +1,15 @@
 import { Component, input, InputSignal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss'
 })
 export class ProductCardComponent {
-  protected productName: InputSignal<string> = input<string>('');
-  protected productBasePrice: InputSignal<number> = input<number>(0);
-  protected productTva: InputSignal<number> = input<number>(0);
-
-  
+  productName: InputSignal<string> = input<string>('Nom');
+  productBasePrice: InputSignal<number> = input<number>(0);
+  productTva: InputSignal<number> = input<number>(0);
 }
