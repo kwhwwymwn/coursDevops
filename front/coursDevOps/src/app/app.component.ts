9e923ccaf6f81bbcae58be321966894c678034ec
julesV <julesvallet4@gmail.com>
2025-03-04 11:05:09 +0100

    search bar component

--- front/coursDevOps/src/app/app.component.ts
+++ front/coursDevOps/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from "./components/product-card/product-card.component";
+import { SearchBarComponent } from "./components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'coursDevOps';
+
+  onSearch(event: any){
+    console.log(event);
+  }
 }
