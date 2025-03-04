0f217885f35f05a2aed6c4f0f1d05bd8bbcdccc5
JoCuadrado <jonathanetude@yahoo.com>
2025-03-04 11:16:51 +0100

    simple header created

--- front/coursDevOps/src/app/app.component.ts
+++ front/coursDevOps/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from "./components/product-card/product-card.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
