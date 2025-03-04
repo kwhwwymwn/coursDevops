70897db53ddaed64ce9f79ffe8595dbaad9bda0f
matheis <m.giammarinaro@groupe-aen.info>
2025-03-04 11:39:30 +0100

    model

--- back/coursDevOps/src/main/java/edu/esiea/coursDevOps/models/Cart.java
+++ back/coursDevOps/src/main/java/edu/esiea/coursDevOps/models/Cart.java
@@ -1,5 +1,67 @@
 package edu.esiea.coursDevOps.models;
 
-public class Cart {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cart {
+	private int id;
+	private int userId;
+	private int quantity;
+	private float totalPrice;
+	private List<Product> products;
+	
+	public Cart(int id, int userId, int quantity, float totalPrice) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.quantity = quantity;
+		this.totalPrice = totalPrice;
+		this.products = new ArrayList<>();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public float getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(float totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void addProduct(Product product) {
+		if(!this.products.contains(product))
+			this.products.add(product);
+	}
+
+	public void removeProduct(Product product) {
+		if(this.products.contains(product))
+			this.products.remove(product);
+	}
 }
--- back/coursDevOps/src/main/java/edu/esiea/coursDevOps/models/Product.java
+++ back/coursDevOps/src/main/java/edu/esiea/coursDevOps/models/Product.java
@@ -1,5 +1,50 @@
 package edu.esiea.coursDevOps.models;
 
 public class Product {
+	private int id;
+	private String name;
+	private float basePrice;
+	private float tva;
+	
+	public Product(int id, String name, float basePrice, float tva) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.basePrice = basePrice;
+		this.tva = tva;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getBasePrice() {
+		return basePrice;
+	}
+
+	public void setBasePrice(float basePrice) {
+		this.basePrice = basePrice;
+	}
+
+	public float getTva() {
+		return tva;
+	}
+
+	public void setTva(float tva) {
+		this.tva = tva;
+	}
+	
+	
 }
