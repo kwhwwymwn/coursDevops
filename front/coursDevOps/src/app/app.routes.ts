49eb9d381d7039c4d681d43ff82273622314b027
julesV <julesvallet4@gmail.com>
2025-03-05 09:56:53 +0100

    page nouveau produit

--- front/coursDevOps/src/app/app.routes.ts
+++ front/coursDevOps/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProductsManagerPageComponent } from './pages/products-manager-page/products-manager-page.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginPageComponent },
+    { path: 'products', component: ProductsManagerPageComponent },
 ];
