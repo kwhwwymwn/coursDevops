84087690d778b2b679a0938dcd746f8ff862e6e7
julesV <julesvallet4@gmail.com>
2025-03-05 15:50:23 +0100

    modif données test

--- front/coursDevOps/src/app/components/cart/cart.component.ts
+++ front/coursDevOps/src/app/components/cart/cart.component.ts
@@ -13,13 +13,13 @@ import { CommonModule } from '@angular/common';
 })
 export class CartComponent {
   products: Product[] = [
-    new Product(2, 149.49, 5, "Popcorn Seed", "https://example.com/popcorn-seed.jpg"),
-    new Product(2, 149.49, 5, "Popcorn Seed", "https://example.com/popcorn-seed.jpg"),
-    new Product(3, 259.99, 5, "Smartphone Tree", "https://example.com/smartphone-tree.jpg"),
-    new Product(5, 187.75, 5, "Pizza Shrub", "https://example.com/pizza-shrub.jpg"),
-    new Product(6, 300.00, 5, "Golden Leaf Plant", "https://example.com/golden-leaf.jpg"),
-    new Product(6, 300.00, 5, "Golden Leaf Plant", "https://example.com/golden-leaf.jpg"),
-    new Product(6, 300.00, 5, "Golden Leaf Plant", "https://example.com/golden-leaf.jpg"),
-    new Product(12, 174.49, 5, "Cloud Berry", "https://example.com/cloud-berry.jpg")
+    new Product(149.49, 5, "Popcorn Seed", "https://example.com/popcorn-seed.jpg"),
+    new Product(149.49, 5, "Popcorn Seed", "https://example.com/popcorn-seed.jpg"),
+    new Product(259.99, 5, "Smartphone Tree", "https://example.com/smartphone-tree.jpg"),
+    new Product(187.75, 5, "Pizza Shrub", "https://example.com/pizza-shrub.jpg"),
+    new Product(300.00, 5, "Golden Leaf Plant", "https://example.com/golden-leaf.jpg"),
+    new Product(300.00, 5, "Golden Leaf Plant", "https://example.com/golden-leaf.jpg"),
+    new Product(300.00, 5, "Golden Leaf Plant", "https://example.com/golden-leaf.jpg"),
+    new Product(174.49, 5, "Cloud Berry", "https://example.com/cloud-berry.jpg")
   ];
 }
--- front/coursDevOps/src/app/components/catalogue/catalogue.component.ts
+++ front/coursDevOps/src/app/components/catalogue/catalogue.component.ts
@@ -14,17 +14,17 @@ import { CommonModule } from '@angular/common';
 })
 export class CatalogueComponent {
   products: Product[] = [
-    new Product(1, 199.99, 5, "Money Tree", "https://example.com/money-tree.jpg"),
-    new Product(2, 149.49, 5, "Popcorn Seed", "https://example.com/popcorn-seed.jpg"),
-    new Product(3, 259.99, 5, "Smartphone Tree", "https://example.com/smartphone-tree.jpg"),
-    new Product(4, 129.99, 5, "Candy Vine", "https://example.com/candy-vine.jpg"),
-    new Product(5, 187.75, 5, "Pizza Shrub", "https://example.com/pizza-shrub.jpg"),
-    new Product(6, 300.00, 5, "Golden Leaf Plant", "https://example.com/golden-leaf.jpg"),
-    new Product(7, 142.20, 5, "Glow-in-the-Dark Cactus", "https://example.com/glow-cactus.jpg"),
-    new Product(8, 225.50, 5, "Chocolate Blossom", "https://example.com/chocolate-blossom.jpg"),
-    new Product(9, 279.99, 5, "WiFi Bush", "https://example.com/wifi-bush.jpg"),
-    new Product(10, 109.99, 5, "Bubblegum Tree", "https://example.com/bubblegum-tree.jpg"),
-    new Product(11, 350.00, 5, "Singing Sunflower", "https://example.com/singing-sunflower.jpg"),
-    new Product(12, 174.49, 5, "Cloud Berry", "https://example.com/cloud-berry.jpg")
+    new Product(199.99, 5, "Money Tree", "https://example.com/money-tree.jpg"),
+    new Product(149.49, 5, "Popcorn Seed", "https://example.com/popcorn-seed.jpg"),
+    new Product(259.99, 5, "Smartphone Tree", "https://example.com/smartphone-tree.jpg"),
+    new Product(129.99, 5, "Candy Vine", "https://example.com/candy-vine.jpg"),
+    new Product(187.75, 5, "Pizza Shrub", "https://example.com/pizza-shrub.jpg"),
+    new Product(300.00, 5, "Golden Leaf Plant", "https://example.com/golden-leaf.jpg"),
+    new Product(142.20, 5, "Glow-in-the-Dark Cactus", "https://example.com/glow-cactus.jpg"),
+    new Product(225.50, 5, "Chocolate Blossom", "https://example.com/chocolate-blossom.jpg"),
+    new Product(279.99, 5, "WiFi Bush", "https://example.com/wifi-bush.jpg"),
+    new Product(109.99, 5, "Bubblegum Tree", "https://example.com/bubblegum-tree.jpg"),
+    new Product(350.00, 5, "Singing Sunflower", "https://example.com/singing-sunflower.jpg"),
+    new Product(174.49, 5, "Cloud Berry", "https://example.com/cloud-berry.jpg")
   ];
 }
