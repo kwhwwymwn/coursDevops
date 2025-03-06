02fb26369b5f9586be9f7b45aa399e1c11c8db56
JoCuadrado <jonathanetude@yahoo.com>
2025-03-06 17:01:15 +0100

    test fonc update + wip cart

--- front/tests/cart.py
+++ front/tests/cart.py
@@ -5,25 +5,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def run():
-    print('header')
+    print('cart')
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options=chrome_options)
-
+    driver = webdriver.Chrome()
     driver.get("http://localhost:4200")
 
     wait = WebDriverWait(driver, timeout=2)
 
-    loginBtn = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-header/div/a[2]/button')))
-    loginBtn.click()
-    wait.until(EC.url_contains("/login"))
-    assert "/login" in driver.current_url
 
-    homeLink = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/app-root/app-header/div/a[1]')))
-    homeLink.click()
-    wait.until(EC.url_contains("/"))
-    assert "/" in driver.current_url
 
     driver.close()
     driver.quit()
--- front/tests/run-test.py
+++ front/tests/run-test.py
@@ -1,7 +1,8 @@
-import header
+import header, cart
 
 print("\n🔍 Lancement de tous les tests Selenium...\n" + "-"*40)
 
 header.run()
+cart.run()
 
 print("\n" + "-"*40 + "\n✅ Tous les tests sont terminés !")
