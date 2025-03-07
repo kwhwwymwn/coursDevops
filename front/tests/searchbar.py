49ddbce828c943f0cf36527fd1943e7a50bcf60a
JoCuadrado <jonathanetude@yahoo.com>
2025-03-07 15:56:57 +0100

    fix searchbar test

--- front/tests/searchbar.py
+++ front/tests/searchbar.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
@@ -8,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def run():
     print("searchbar")
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options=chrome_options)
 
@@ -35,8 +36,8 @@ def run():
 
         assert found, f"The product '{existing_product}' was not found after search."
 
-    except Exception as e:
-        assert False, f"Error in the first test: {e}"
+    except:
+        assert False, "Error in the first test"
 
     # Searching a non existing product with the search button
     try:
@@ -50,22 +51,23 @@ def run():
         product_cards = driver.find_elements(By.XPATH, "//app-product-card")
         assert len(product_cards) == 0, f"No product should match, but product cards are present after search for '{non_existing_product}'."
 
-    except Exception as e:
-        assert False, f"Error in the second test: {e}"
+    except:
+        assert False, "Error in the second test"
 
     # Empty search with the search button
     try:
         search_input = wait.until(EC.presence_of_element_located((By.XPATH, "//app-search-bar//input")))
         search_button = wait.until(EC.presence_of_element_located((By.XPATH, "//app-search-bar//button")))
 
-        search_input.clear()
+        search_input.send_keys(Keys.CONTROL + "a")
+        search_input.send_keys(Keys.BACKSPACE)
         search_button.click()
 
-        product_cards = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//app-product-card")))
+        product_cards = driver.find_elements(By.XPATH, "//app-product-card")
         assert len(product_cards) == 3, f"Expected to found 3 cards, but found {len(product_cards)}."
 
-    except Exception as e:
-        assert False, f"Error in the third test: {e}"
+    except:
+        assert False, "Error in the third test"
 
 
     driver.close()
